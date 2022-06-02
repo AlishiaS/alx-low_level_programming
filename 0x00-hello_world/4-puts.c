0eb5cac810ea3d865c23dbb9d80250614f36f65a
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-02 07:50:22 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 /**
+ * main - returns  
  *
- *  * main - returns 0
+ * Description - main returns 0
  *
+ * puts - Writes string and newline to stdout
  *
- *  * Description - main returns 0
- *
- *  * puts - Writes string and newline to stdout
- *
- *  * Return: 0
- *
- *      
+ * Return: 0
  */
 int main(void)
 {
