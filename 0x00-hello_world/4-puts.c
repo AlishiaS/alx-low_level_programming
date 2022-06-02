7648624695b85c45ea9e59afa4c31458d9d4d949
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-02 07:04:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,25 @@
 #!/bin/bash
 #include <stdio.h>
 
+/**
+ *
+ *  * main - returns 0
+ *
+ *   *
+ *
+ *    * Description - main returns 0
+ *
+ *     * puts - Writes string and newline to stdout
+ *
+ *      * Return: 0
+ *
+ *       */
+
 int main(void)
 
 {
 
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
