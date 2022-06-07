e8af12084aa625cc78b7a5a344d8c37674ddbc49
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-07 16:07:08 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,20 +7,27 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
+	int a;
+	char b;
+
+	if (n <= 0)
 	{
-		_putchar('+');
-		return (1);
+		if (n == 0)
+		{
+			a = 0;
+			b = '0';
+		}
+		else
+		{
+			a = -1;
+			b = '-';
+		}
 	}
-	else if (n == 0)
+	else
 	{
-		_putchar('0');
-		return (1);
+		a = 1;
+		b = '+';
 	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	return (0);
+	_putchar(b);
+	return (a);
 }
