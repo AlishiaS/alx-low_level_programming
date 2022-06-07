ac736cc3631db3d36379f4e4c7367ed9a1c44517
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-07 15:11:31 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,8 @@
 /**
  *  _islower - Function prototype
  *  Description -   checks for lowercase character
- *  Return: 1 if lowercase otherwise 0 
+ *   @c: letter being tested
+ *  Return: 1 if lowercase otherwise 0
  */
 int _islower(int c)
 {
