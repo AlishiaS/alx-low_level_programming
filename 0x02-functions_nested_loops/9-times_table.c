94a991564292c5bfb0a555f91d221e5b48774feb
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-08 05:52:05 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  */
 void times_table(void)
 {
-	int a, y, num;
+	int a, c, num;
 
 	for (a = 0; a <= 9; a++)
 	{
@@ -26,8 +26,8 @@ void times_table(void)
 
 			else if (num >= 10)
 			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
 			}
 
 			else if ((num < 10) && (c != 0))
