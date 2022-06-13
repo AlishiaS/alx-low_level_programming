f9ee20e4b8d51aa15de1fefbd3fec10fa9ce0700
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-13 15:14:50 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to print
+ * Return: void
+ */
+void puts2(char *str)
+{
+	for(int i = 0; i < strlen(input); i+=2)
+	{
+		word[i/2] = input[i];
+	}
+	_putchar('\n');
+
+	return 0;
+}
