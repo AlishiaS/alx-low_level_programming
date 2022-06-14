fd9e054dbb5cbab8f712b95325b769db0c316e7b
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-14 14:51:55 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,12 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	int n;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
-	while (i < n)
-	{
+	
 		dest[i] = '\0';
 		i++;
-	}
+	
 	return (dest);
 }
