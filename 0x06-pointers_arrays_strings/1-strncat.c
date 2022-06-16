ce80afd27ce35737826627be5780cab5ca7a75c0
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-16 13:43:27 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,12 @@
  */
 char *_strncat(char *dest, char *src, int n);
 {
-	int a, i;
-	
-	for (a = 0; dest[a] != '\0'; a++)
-		;
+	int a, i
+		
+	for (a = 0 ; a < n && src[i] != '\0' ; i++)
+		dest[a] = src[i];
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[i] = src[i];
-
+	for (i = 0; i < n; i++)
 		dest[i] = '\0';
 	
 	return (dest);
