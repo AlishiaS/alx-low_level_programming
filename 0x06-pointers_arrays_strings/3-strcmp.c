0bf3d5e7692247296245c2db1c4c500c5160c9cf
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-16 10:36:41 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,15 +4,15 @@
  * Description - compares two strings
  * @s1 : String 1
  * @s2 : String 2
- * Return : 0 if flag is 0, else return 1
+ * Return : flag
  */
 int _strcmp(char *s1, char *s2)
 {
 	int flag = 0;
 
-	while(*s1 != '\0' && *s2 != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if(*s1 != *s2)
+		if (*s1 != *s2)
 		{
 			flag = 1;
 		}
@@ -21,8 +21,8 @@ int _strcmp(char *s1, char *s2)
 	}
 
 	if (flag == 0)
-		return 0;
+		return (0);
 	else
-		return 1;
+		return (1);
 }
 
