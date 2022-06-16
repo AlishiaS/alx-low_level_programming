cff446383126d59ac067a45536478d774540ed96
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-16 15:25:29 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - Function Prototype
+ * Description - apitalizes all words of a string
+ * @s : String to be changed
+ * Return: Pointer to the string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int spaceCount = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((spaceCount %2 == 1) && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+		else if(s[i] == ',')
+		{
+			spaceCount++;
+		}
+		i++;
+	}
+}
