18e9c6cc3562411d257ee0601088aec31f32a2bc
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-16 10:28:32 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - Function Prototype
+ * Description - compares two strings
+ * @s1 : String 1
+ * @s2 : String 2
+ * Return : 0 if flag is 0, else return 1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int flag = 0;
+
+	while(*s1 != '\0' && *s2 != '\0')
+	{
+		if(*s1 != *s2)
+		{
+			flag = 1;
+		}
+		s1++;
+		s2++;
+	}
+
+	if (flag == 0)
+		return 0;
+	else
+		return 1;
+}
+
