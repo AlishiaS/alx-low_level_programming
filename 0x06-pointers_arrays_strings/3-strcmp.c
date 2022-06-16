22c68b4de9ac1e8bec58045ccb2ab7b5c99c04e1
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-16 11:32:23 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 == '\0' || *s2 == '\0')
-			break;
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
-	if (*s1 == '\0' && *s2 == '\0')
+	if (s1[i] == s2[i])
 		return (0);
-	else
-		return (-1);
+	return (s1[i] - s2[i]);
 }
