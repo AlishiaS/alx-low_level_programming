2f4705ab2031fc31f1496f1a93ccf1207e3d2045
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-16 14:48:10 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - Function Prototype
+ * Description - changes all lowercase letters of a string to uppercase
+ * @s : String to be changed
+ * Return : :Pointer to the s
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return s;
+}
