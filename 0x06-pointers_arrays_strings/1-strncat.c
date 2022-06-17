d669d766269c12546df53e6d1474e5bdc6caa1e1
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-17 09:01:59 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,22 +4,25 @@
 /**
  * _strncat - concatenates two strings
  * @n: number of bytes
- * @dest :Pointer to Destination string
- * @src :Pointer to Source string
- * Return: pointer to destination string
+ * @dest: Pointer to Destination string
+ * @src: Pointer to Source string
+ * Return: The pointer to destination string
  */
 char *_strncat(char *dest, char *src, int n);
 {
 	int i;
+	int a;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	for (a = 0; dest[a]; a++)
 	{
-		dest[i] = src[i];
 	}
 
-	for (; i < n; i++)
+	for (i = 0; i < n && dest[i]; i++)
 	{
-		dest[i] = '\0';
+		dest[i + a] = src[i];
 	}
+
+	dest[i + a] = '\0';
+
 	return (dest);
 }
