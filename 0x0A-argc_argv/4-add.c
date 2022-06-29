c5b94474921749ec48674b074f37f9682b362408
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-29 12:03:10 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - adds positive numbers.
+ * @argc: number of arguments
+ * @argv: array of pointers
+ * Return: Error, 1 - If one of the number contains symbols that are not digits,
+ *	0 - If no number is passed to the program.
+ */
+int main(int argc, char *argv[])
+{
+	int n, d, sum = 0;
+
+	for (n =1; n < argc; n++)
+	{
+		for (d = 0; argv[n][d]; d++)
+		{
+			if (argv[n][d] < '0' || argv[n][d] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[n]);
+	}
+	printf("%d\n", sum)
+
+	return (0)
+}
