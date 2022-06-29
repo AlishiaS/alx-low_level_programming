15bf4dec2f8805fea8d190c35b95f8f4dd31cbfe
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-06-29 13:57:04 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,15 +5,20 @@
  * @argv: array of pointers
  * Return: 0 if two arguments are passed, else 0.
  */
-int main(const int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
+	int n1, n2, result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
+		return(1);
 	}
 
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	result = n1 * n2;
 
+	printf("%d\n", result)
 	return (o);
 }
