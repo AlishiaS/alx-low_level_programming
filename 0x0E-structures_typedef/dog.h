b536897b9dfbd41a0a07954e05d68064599b0d07
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-07-04 12:44:59 -0700

    Dog main file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - structure definition of a dog
  * @name: character string
@@ -12,9 +11,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
