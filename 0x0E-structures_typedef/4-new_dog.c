1e47dc7fa24577beb0a3eede897238373afb5d47
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-07-05 12:41:58 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,29 +12,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int len_name, len_owner;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
 	new_dog = malloc(sizeof(dog_t));
-
 	if (new_dog == NULL)
 		return (NULL);
-	
-	len_name = _strlen(name);
 
-	new_dog->name = malloc(sizeof(char) * len_name + 1);
-
+	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	new_dog->name = _strcpy(new_dog->name, name);
-
-	len_owner = _strlen(owner);
-
-	new_dog->owner = malloc(sizeof(char) * len_owner + 1);
-
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
@@ -42,8 +35,46 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->owner = _strcpy(new_dog->owner, owner);
+	new_dog->name = _strcopy(new_dog->name, name);
 	new_dog->age = age;
+	new_dog->owner = _strcopy(new_dog->owner, owner);
 
 	return (new_dog);
+}
+
+
+/**
+ * _strlen - determinates the length of a string
+ * @s: pointer to string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+
+	return (length);
+}
+
+
+/**
+ * _strcpy - copies a pointed string
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * Return: address of the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int b = 0;
+
+	while (src[b] != '\0')
+	{
+		dest[] = src[b];
+		b++;
+	}
+	dest[b] = '\0';
+
+	return (dest);
 }
