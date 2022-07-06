e620750788eff13c49eceee1b28f3d3c5b4b7d2a
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-07-06 11:56:00 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * @name: string
  * @f: function
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
