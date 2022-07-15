fd35f0bb6eb959b47cc4a847ab0ffd3be5c90dc4
AlishiaS <subramoneyalishia@gmail.com>
2022-07-16 00:58:26 +0200

    Revert "Your subconscious is looking for the dreamer"
    This reverts commit 58b5ec5f274f18d0daec579d9ce0df5de1705750.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,4 @@
 #include "main.h"
-
-int _sqrt_recursion(int n);
-int _sqrt(int n, int r);
-
 /**
  * _sqrt_recursion - returns the natural sqare root of a number
  * @n: number to square
@@ -19,12 +15,12 @@ int _sqrt_recursion(int n)
  * @r: square root
  * Return: square root of number.
  */
-int _sqrt(int n, int r)
+int sqrt(int n, int r)
 {
 	if (r * r == n)
 		return (r);
 	else if (r * r < n)
-		return  (_sqrt(n, r + 1));
+		return  (square(n, r + 1));
 	else
 		return (-1);
 }
