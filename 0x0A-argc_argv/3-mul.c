838884b38ffa9b71fae8d31c00564ef49bd8712b
AlishiaS <subramoneyalishia@gmail.com>
2022-07-15 23:19:58 +0200

    Update 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multilpies two numbers
+ * @argc: number of arguments
+ * @argv: array of pointers
+ * Return: 0 if two arguments are passed, else 0.
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return(1);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	result = n1 * n2;
+
+	printf("%d\n", result)
+	return (o);
+}
