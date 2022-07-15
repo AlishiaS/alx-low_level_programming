32815309b18aa9433d90a5c79a33005913b51d2f
AlishiaS <subramoneyalishia@gmail.com>
2022-07-15 23:20:17 +0200

    Revert "array_range"
    This reverts commit 32d8783fee8b12071b07815d16e805dcf31542c1.

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,20 +9,19 @@
  */
 int *array_range(int min, int max);
 {
-	int *n, a, size;
+	int *n;
+	int a;
 
 	if (min > max)
 		return (NULL);
 
-	size = max - min + 1;
-
-	n = malloc(sizeof(int) * size);
+	n = malloc(sizeof(int) * (max - min + 1));
 
 	if (n == NULL)
 		return (NULL);
 
-	for (a = 0; a < size; a++)
-		n[a] = min++;
+	for (a = 0; min + a <= max; a++)
+		n[a] = min + 1;
 	
 	return (n);
 }
