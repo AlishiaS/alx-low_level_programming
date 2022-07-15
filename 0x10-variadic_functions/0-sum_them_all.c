788449f87c405c9355bf5b9e8fb7bf2053bcd259
AlishiaS <subramoneyalishia@gmail.com>
2022-07-15 23:58:18 +0200

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,13 +15,18 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-	va_start(list, n);
+	va_start (list, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
+<<<<<<< HEAD
 	{
 		sum += va_arg(list, int);
 	}
 	va_end(list);
+=======
+		sum += va_arg (list, int);
+	va_end (list);
+>>>>>>> parent of daffa77 (Beauty is variable, ugliness is constant)
 
 	return (sum);
 }
