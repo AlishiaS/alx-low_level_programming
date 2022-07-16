df44a277ad3b0c5e7476451d6557e4da82585d9f
AlishiaS <subramoneyalishia@gmail.com>
2022-07-16 15:26:57 +0200

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,12 +7,18 @@
  */
 int main(const int argc, char const *argv[])
 {
+<<<<<<< HEAD
+=======
+	(void)argc;
+
+>>>>>>> parent of d9c092e (Neither irony nor sarcasm is argument)
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
+<<<<<<< HEAD
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 <<<<<<< HEAD
@@ -20,6 +26,9 @@ int main(const int argc, char const *argv[])
 	printf("%d\n", result)
 =======
 	printf("%d\n", result);
+=======
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+>>>>>>> parent of d9c092e (Neither irony nor sarcasm is argument)
 
 >>>>>>> parent of 50787ce (Neither irony nor sarcasm is argument)
 =======
