9305e6d186e455fd2101475b20793ad14ebc0e38
AlishiaS <subramoneyalishia@gmail.com>
2022-07-16 15:57:54 +0200

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strncpy - Function Prototype
+ * Description - copies a string
+ * @n: int
+ * @dest: Pointer to destination string
+ * @src :Pointer to source string
+ * Return: Always 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++;)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
