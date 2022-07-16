979e63620f781e509c0b9562b384647d30120b2f
AlishiaS <subramoneyalishia@gmail.com>
2022-07-16 15:08:10 +0200

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,15 +10,20 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	char *s;
 =======
 	char *str;
 >>>>>>> parent of 553dbaa (_calloc)
+=======
+	char *s;
+>>>>>>> parent of 8e457f2 (_calloc)
 	unsigned int a;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 	s = malloc(size * nmemb);
 
@@ -29,11 +34,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (str == NULL)
 	{
 >>>>>>> parent of 553dbaa (_calloc)
+=======
+	s = malloc(size * nmemb);
+
+	if (s == NULL)
+>>>>>>> parent of 8e457f2 (_calloc)
 		return (NULL);
-	}
 
 	a = 0;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -52,11 +62,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	return (s);
 =======
 	while (str < (nmemb * size))
+=======
+	while (s < (nmemb * size))
+>>>>>>> parent of 8e457f2 (_calloc)
 	{
-		str[a] = 0;
+		s[a] = 0;
 		a++;
 	}
 
+<<<<<<< HEAD
 	return (str);
 >>>>>>> parent of 553dbaa (_calloc)
+=======
+	return (s);
+>>>>>>> parent of 8e457f2 (_calloc)
 }
