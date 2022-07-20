34de2574e1df81c1a049c2d347de7b55d7f24cd2
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-07-20 13:12:17 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,39 +11,23 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
 	listint_t *new;
-	listint_t *h;
 
-	h = *head;
-
-	if (idx != 0)
-	{
-		for (i = 0; i < idx - 1 && h != NULL; i++)
-		{
-			h = h->next;
-		}
-	}
-
-	if (h == NULL && idx != 0)
+	if (!head)
+		return (NULL);
+	if (idx && *head)
+		return (insert_nodeint_at_index(&(*head)->next, idx - 1, n));
+	if (idx)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
-	if (n == NULL)
+	if (!new)
 		return (NULL);
 
 	new->n = n;
+	new->next = *head;
 
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		new->next = h->next;
-		h->next = new;
-	}
+	*head = new;
 
 	return (new);
 }
