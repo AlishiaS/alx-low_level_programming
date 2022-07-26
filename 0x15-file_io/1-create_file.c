c2946075b48e35d6bed0351114cadd55339eab9f
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-07-26 12:43:39 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,21 +9,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr, length = 0;
+	int op, wr, length;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (length = 0; text_content[length];)
-			length++;
-	}
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	op = open(filename, O_CREATE | O_RDWR | O_TRUNC, 600);
+	if (op == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (length = 0; text_content[length]; length++)
+		;
+
 	wr = write(op, text_content, length);
 
-	if (op == -1 || wr == -1)
+	if (wr == -1)
 		return (-1);
 
 	close(op);
