82c13ceb4ec1176f0ebf0e3c2c9d13eb8ac0e94e
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-09-27 09:18:24 -0700

    main

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,4 +1,4 @@
-#ifndef HASH_TABLES_H 
+#ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
 #include <stdlib.h>
@@ -43,4 +43,4 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-#endif
+#endi f
