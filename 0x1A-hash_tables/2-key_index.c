787ee8e4d00ac18f26b24279691320e5c24ad764
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-09-28 09:29:37 -0700

    key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int idx = hash_djb2(key) % size;
-
-	return (idx);
+       return (hash_djb2(key) % size);
 }
