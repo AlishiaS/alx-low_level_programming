c6f8b97aaca08ac0227b46abdb5a6f2cba3a38b8
Alishia Subramoney <subramoneyalishia@gmail.com>
2023-02-02 18:00:29 +0200

    Commit file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,29 +1,30 @@
 #include "lists.h"
 
 /**
- * *insert_dnodeint_at_index - inserts a new node at a given position
- * @h: head of list
- * @idx: index
- * @n: number
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ * list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
  *
- * Return: the address of the new node,
- *	Otherwise - NULL if it failed.
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tm = *h, *new;
+	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
 	for (; idx != 1; idx--)
 	{
-		tm = tm->next;
-		if (tm == NULL)
+		tmp = tmp->next;
+		if (tmp == NULL)
 			return (NULL);
 	}
 
-	if (tm->next == NULL)
+	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
@@ -31,10 +32,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	new->prev = tm;
-	new->next = tm->next;
-	tm->next->prev = new;
-	tm->next = new;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
 	return (new);
 }
