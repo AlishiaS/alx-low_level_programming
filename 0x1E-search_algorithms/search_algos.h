b0f36c0870d3842feffb6b32f5fbc01222037103
Alishia Subramoney <subramoneyalishia@gmail.com>
2023-02-07 20:29:05 +0200

    Commit file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,17 +7,18 @@
 
 /**
  * struct listint_s - singly linked list
+ *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  *
- * Desc: singly linked list node structure
+ * Description: singly linked list node structure
  */
 typedef struct listint_s
 {
     int n;
-	size_t index;
-	struct listint_s *next;
+    size_t index;
+    struct listint_s *next;
 } listint_t;
 
 /**
